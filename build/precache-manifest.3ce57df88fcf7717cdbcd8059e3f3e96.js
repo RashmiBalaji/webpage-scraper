55f96956165519af85337f55e0ce4fe70a6d25ac
Balaji <balaji.ramamurthy@me.com>
2020-11-07 14:19:52 +0100

    build files

---
+++ build/precache-manifest.3ce57df88fcf7717cdbcd8059e3f3e96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9bf743b0a1581d78659d58da22fe8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfaa798253baed8afab5",
+    "url": "/static/css/main.f34c7558.chunk.css"
+  },
+  {
+    "revision": "50bd567667122daff9ee",
+    "url": "/static/js/2.25ff6dfe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.25ff6dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfaa798253baed8afab5",
+    "url": "/static/js/main.15487bcf.chunk.js"
+  },
+  {
+    "revision": "4d8e67a1be211c585fec",
+    "url": "/static/js/runtime-main.c4794220.js"
+  }
+]);
